2f7e4e486ca98f5c04964b8173332a49c9f0823e
Satana Charuwichitratana <satana.charu@gmail.com>
2022-07-31 17:13:00 +0700

    fix: Mod incorrectly always adding a katana after loaded

--- Contents/mods/MichonnesKatana/media/lua/server/MichonnesKatana.lua
+++ Contents/mods/MichonnesKatana/media/lua/server/MichonnesKatana.lua
@@ -1,12 +1,18 @@
 MichonnesKatana = {}
 
 function MichonnesKatana.onCreatePlayer(index, player)
-  if player:getHoursSurvived() > 1 then
+  if (player:getModData().addedKatana == true) then
     return
   end
 
   local inventory = player:getInventory()
   inventory:AddItem("Michonne.Katana")
+  player:getModData().addedKatana = true
+end
+
+function MichonnesKatana.onPlayerDeath(player)
+  player:getModData().addedKatana = false
 end
 
 Events.OnCreatePlayer.Add(MichonnesKatana.onCreatePlayer);
+Events.OnPlayerDeath.Add(MichonnesKatana.onPlayerDeath)
